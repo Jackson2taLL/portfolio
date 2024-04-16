05287ab2e9f026deae47a738a247d502ae0cb6eb
Jackson2taLL <jacksonkta10@gmail.com>
2024-04-16 12:17:35 -0700

    add Outlet component from react router dom and include top and bottom tags

--- my-portfolio-website/src/components/Layout/Layout.jsx
+++ my-portfolio-website/src/components/Layout/Layout.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Layout.scss'
 import Sidebar from '../Sidebar/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = (props) => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags ottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
